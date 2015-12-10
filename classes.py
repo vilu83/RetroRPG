c05ab12fb27186d36dab6670c58599750835ece8
vilu83 <vilu83@users.noreply.github.com>
2015-12-10 12:33:29 -0300

    Create classes.py

---
+++ classes.py
@@ -0,0 +1,21 @@
+class character(object):
+	lv = 1
+	hp = 100
+	atk = 1
+	Def = 1
+	Luck = 1
+	Speed = 1
+	Doge = 1			#such Doge, much wow, very Speedy
+	Crit_ch = 1		
+	#mana = ''
+	#mana_re = ''
+	WP1 = ''
+	WP2 = ''
+	helmet = ''
+	armor = ''
+	boots = ''
+	exp_missing = 100
+	exp_gained =''
+	#if exp_missing == 0:
+	#	self.hp +=
+	#	self 
